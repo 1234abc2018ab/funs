1b5ee2a26911f4de82e9f49bf1e1187dd957cf6f
1234abc2018ab <42770682+1234abc2018ab@users.noreply.github.com>
2018-08-29 20:11:06 +0800

    Update tochange.js

--- tochange.js
+++ tochange.js
@@ -1,5 +1,5 @@
  function func1() {
- var re_arr=[{'asdsdd','chy99','2018-01-01'},{'bbc','chy99','2018-11-01'}];
+ var re_arr=[['asdsdd','chy99','2018-01-01'],['bbc','chy99','2018-11-01']];
  // return "ff";
   return re_arr;
    };
