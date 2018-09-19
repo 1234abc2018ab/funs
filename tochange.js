5b45921b31579b9967a1bac05361b3449b06a41a
1234abc2018ab <42770682+1234abc2018ab@users.noreply.github.com>
2018-09-19 10:54:12 +0800

    Update tochange.js

--- tochange.js
+++ tochange.js
@@ -2,7 +2,7 @@
  var re_arr =[
 		{id:'a2123aa',name:'chy99',date:'2018-01-01'}
 		,{id:'aa',name:'chy99',date:'2018-11-01'}
-	       ,{id:'aa2',name:'ji000',date:'2018-11-01'}
+	       ,{id:'aa2',name:'cai000',date:'2018-11-01'}
 	       ,{id:'aa3',name:'ji000',date:'2018-11-01'}
 	       ,{id:'a20',name:'ji000',date:'2018-11-11'}
 	       ,{id:'a31',name:'2693',date:'2018-11-11'}
