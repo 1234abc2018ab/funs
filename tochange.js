245ab30952f64a692792a0cef8b065897fecd70b
1234abc2018ab <42770682+1234abc2018ab@users.noreply.github.com>
2018-10-02 17:42:45 +0800

    Update tochange.js

--- tochange.js
+++ tochange.js
@@ -5,10 +5,10 @@
 	      ,{id:'aa1',name:'cai000',date:'2018-11-01'}
 	       ,{id:'aa2',name:'ca000',date:'2018-11-01'}
 	       ,{id:'aa3',name:'ji000',date:'2018-11-01'}
-	       ,{id:'a20',name:'ji000',date:'2018-11-11'}
+	       ,{id:'a20',name:'cai777',date:'2018-11-11'}
 	       ,{id:'a31',name:'2693',date:'2018-11-11'}
 	       ,{id:'a32',name:'8670',date:'2018-11-11'}
-	,{id:'a36',name:'cai666',date:'2018-11-11'}
+	,{id:'a36',name:'cai777',date:'2018-11-11'}
 	  ]; 
   return re_arr;
    };
